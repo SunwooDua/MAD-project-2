80272216d43141d6beca27425bb9ef8fea5f584b
Sunwoo Yeom <o55306605@gmail.com>
2025-04-16 14:04:10 -0400

    base work for api services

---
+++ lib/services/api_service.dart
@@ -0,0 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String API_KEY = 'f276def9aa8fec1dab67ecf5b3b84378';
+  final String baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+}
