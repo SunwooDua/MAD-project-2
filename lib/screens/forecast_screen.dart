6af158919b587012440ba336163b892b1431de80
Sunwoo Yeom <o55306605@gmail.com>
2025-04-16 14:23:41 -0400

    minor update

--- lib/screens/forecast_screen.dart
+++ lib/screens/forecast_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project2/services/api_service.dart';
 
 class ForecastScreen extends StatefulWidget {
   final String latitude;
@@ -15,6 +16,7 @@ class ForecastScreen extends StatefulWidget {
 }
 
 class _ForecastScreenState extends State<ForecastScreen> {
+  final _weatherService = WeatherService();
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,22 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:project2/models/weathers.dart';
 
-class ApiService {
+class WeatherService {
   final String API_KEY = 'f276def9aa8fec1dab67ecf5b3b84378';
   final String baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  Future<Weather> fetchWeather(String location) async {
+    // from inclass 15
+    final response = await http.get(
+      Uri.parse('$baseUrl?q=$location&appid=$API_KEY&units=metric'),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return Weather.fromJson(data);
+    } else {
+      throw Exception('Failed to load weather');
+    }
+  }
 }
