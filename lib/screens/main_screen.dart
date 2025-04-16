adfebf4382b919d72adfe26f6de2e0d7a9e6c74b
Sunwoo Yeom <o55306605@gmail.com>
2025-04-16 12:43:21 -0400

    basic ui + permisssion

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -8,11 +8,52 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  final TextEditingController _locationController = TextEditingController();
+  String location = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Weather App')),
-      body: Column(children: [Text("This is main screen")]),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              child: Center(
+                child: Text(location, style: TextStyle(fontSize: 25)),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _locationController,
+              decoration: InputDecoration(
+                labelText: 'Enter your location',
+                border: OutlineInputBorder(), // border
+                suffixIcon: IconButton(
+                  // button to enter location
+                  onPressed: () {
+                    //update location manually when push button
+                    setState(() {
+                      location = _locationController.text;
+                    });
+                  },
+                  icon: Icon(Icons.add_location),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Auto-detect Location'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
