55c77f0211a3ffe9792001a4d1cc26ae440ceb37
Sunwoo Yeom <o55306605@gmail.com>
2025-04-17 21:10:03 -0400

    initial setup

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:project2/services/api_service.dart';
 import 'package:project2/models/weathers.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
