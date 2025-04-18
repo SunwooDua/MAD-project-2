67163c05fad527fc4aa0ec0008104a57fa5f22ca
Sunwoo Yeom <o55306605@gmail.com>
2025-04-18 12:08:25 -0400

    added firebase package, setup toggle button for personalized alert

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:project2/services/api_service.dart';
 import 'package:project2/models/weathers.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -10,6 +14,53 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  // Theme
+  String? _selectTheme;
+
+  // alert
+  bool _rainAlert = false; // initially false
+  bool _snowAlert = false;
+  double _temperatureThreshold = 50; // initial temperature
+
+  // load alert function
+  Future<void> _loadAlert() async {
+    // just to simplify
+    DocumentSnapshot doc =
+        await FirebaseFirestore.instance
+            .collection('settings')
+            .doc('default')
+            .get();
+
+    // only when doc exist
+    if (doc.exists) {
+      Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+      var alert =
+          data['alerts'] ??
+          {}; // update alert, when null update as empty string
+      setState(() {
+        _rainAlert = alert['rain'] ?? false;
+        _snowAlert = alert['snow'] ?? false;
+        _temperatureThreshold = (alert['temperature'] ?? 50).toDouble();
+      });
+    }
+  }
+
+  // update alert function
+  Future<void> _updateAlert() async {
+    FirebaseFirestore.instance.collection('settings').doc('default').set({
+      // using default since no login is required
+      'alerts': {
+        'rain': _rainAlert,
+        'snow': _snowAlert,
+        'temperature': _temperatureThreshold,
+      },
+    }, SetOptions(merge: true)); // only change data mentioned above to true!
+
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text('Alert Setting has been updated')));
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
